5d1f90d273abdb61816a25322de15c5ef717fa7f
Alexander Semenov <AlexPskDesign@yandex.ru>
2026-09-04 13:06:33 +0300

    try different yandex id

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
 	<StrictMode>
-		<Analytic.Provider value={{ yandexId: 96728508, googleId: 'G-ZXWBHB1NR0' }}>
+		<Analytic.Provider value={{ yandexId: 112285605, googleId: 'G-ZXWBHB1NR0' }}>
 			<BrowserRouter
 				basename={process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/'}>
 				<App />
