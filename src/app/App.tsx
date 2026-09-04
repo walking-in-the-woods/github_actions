5d547254909e0e97575d0f6e948b9fd7a25ea152
Alexander Semenov <AlexPskDesign@yandex.ru>
2026-09-04 13:58:34 +0300

    fix: change button id

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -24,7 +24,7 @@ export const App = () => {
 				</Link>
 				<button
 					className={styles.link}
-					onClick={() => (window as any).ym(112285605, 'reachGoal', 'buy')}>
+					onClick={() => (window as any).ym(608787683, 'reachGoal', 'buy')}>
 					Купить
 				</button>
 			</div>
